2df7e228e1f97a9f1bb4cee9192d3ecf394446f7
JC-NYCC-1227 <jchei@council.nyc.gov>
2018-04-18 15:17:50 -0400

    icons

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -22,26 +22,27 @@ $(window).on("load",function(){
 
   $(window).on("click",function(){
     if(scrolling){
-      $("#scroll-status").html("paused");
+      $("#scroll-status").html("paused <i class='fas fa-pause v-middle'></i>").css({"background-color":"rgba(194,0,26,.5)"});
       clearTimeout(stop);
       $('html, body').stop();
       stop = setTimeout(function(){
         scrollToBottom();
-        $("#scroll-status").html("on");
+        $("#scroll-status").html("on <i class='fas fa-play v-middle'></i>").css({"background-color":"rgba(38,171,1,.5)"});
       },10000);
     };
   });
   
-  $("#seal-header img").click(function(e){
+  $("#scroll").click(function(e){
     e.stopPropagation();
     scrolling = !scrolling;
     if(scrolling){
-      $("#scroll-status").html("on");
+      $("#scroll-status").html("on <i class='fas fa-play v-middle'></i>").css({"background-color":"rgba(38,171,1,.5)"});
       scrollToBottom();
     } else {
       clearTimeout(stop);
       $('html, body').stop();
-      $("#scroll-status").html("off");
+      $("#scroll-status").html("off <i class='fas fa-stop v-middle'></i>").css({"background-color":"rgba(194,0,26,.5)"});
     };
   });
+  $("#scroll").trigger("click");
 });
