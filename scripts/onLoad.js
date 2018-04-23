861f0dbe2b69c4b1e346b1feb27703295f8e4530
JC-NYCC-1227 <jchei@council.nyc.gov>
2018-04-23 16:15:18 -0400

    jquery mobile

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -30,6 +30,18 @@ $(window).on("load",function(){
       },10000);
     };
   });
+
+  $(window).on("tap",function(){
+    if(scrolling){
+      $("#scroll-status").html("paused <i class='fas fa-pause v-middle'></i>").css({"background-color":"rgba(194,0,26,.5)"});
+      clearTimeout(stop);
+      $('html, body').stop();
+      stop = setTimeout(function(){
+        scrollToBottom();
+        $("#scroll-status").html("on <i class='fas fa-play v-middle'></i>").css({"background-color":"rgba(38,171,1,.5)"});
+      },10000);
+    };
+  });
   
   $("#scroll").click(function(e){
     e.stopPropagation();
@@ -43,6 +55,21 @@ $(window).on("load",function(){
       $("#scroll-status").html("off <i class='fas fa-stop v-middle'></i>").css({"background-color":"rgba(194,0,26,.5)"});
     };
   });
+
+    
+  $("#scroll").on("tap",function(e){
+    e.stopPropagation();
+    scrolling = !scrolling;
+    if(scrolling){
+      $("#scroll-status").html("on <i class='fas fa-play v-middle'></i>").css({"background-color":"rgba(38,171,1,.5)"});
+      scrollToBottom();
+    } else {
+      clearTimeout(stop);
+      $('html, body').stop();
+      $("#scroll-status").html("off <i class='fas fa-stop v-middle'></i>").css({"background-color":"rgba(194,0,26,.5)"});
+    };
+  });
+
   setTimeout(function(){
     $(window).scrollTop(0);
     $("#scroll").trigger("click");
