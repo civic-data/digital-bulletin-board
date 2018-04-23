a1c5527247bdd5982238c91b589cad861e2ba8f0
JC-NYCC-1227 <jchei@council.nyc.gov>
2018-04-23 16:27:53 -0400

    alter shit

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -31,7 +31,7 @@ $(window).on("load",function(){
     };
   });
 
-  $(window).on("tap",function(){
+  $(window).on("touchmove",function(){
     if(scrolling){
       $("#scroll-status").html("paused <i class='fas fa-pause v-middle'></i>").css({"background-color":"rgba(194,0,26,.5)"});
       clearTimeout(stop);
@@ -44,20 +44,6 @@ $(window).on("load",function(){
   });
   
   $("#scroll").click(function(e){
-    e.stopPropagation();
-    scrolling = !scrolling;
-    if(scrolling){
-      $("#scroll-status").html("on <i class='fas fa-play v-middle'></i>").css({"background-color":"rgba(38,171,1,.5)"});
-      scrollToBottom();
-    } else {
-      clearTimeout(stop);
-      $('html, body').stop();
-      $("#scroll-status").html("off <i class='fas fa-stop v-middle'></i>").css({"background-color":"rgba(194,0,26,.5)"});
-    };
-  });
-
-    
-  $("#scroll").on("tap",function(e){
     e.stopPropagation();
     scrolling = !scrolling;
     if(scrolling){
