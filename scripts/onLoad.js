f517f1129f3b161f05922daa4129353def8d62ad
JC-NYCC-1227 <jchei@council.nyc.gov>
2018-04-25 14:00:44 -0400

    reload at end not every hour

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -3,9 +3,9 @@ $(window).on("load",function(){
   var windowOpenTime = new Date();
   var refreshTime = new Date(windowOpenTime.getFullYear(),windowOpenTime.getMonth(),windowOpenTime.getDate(),(windowOpenTime.getHours()+1));
   var countdown = refreshTime.getTime() - windowOpenTime.getTime();
-  setTimeout(function(){
-    window.location.href = window.location.href;
-  },countdown);
+  // setTimeout(function(){
+  //   window.location.href = window.location.href;
+  // },countdown);
 
   function scrollToBottom(){
     $('html, body').animate({ 
